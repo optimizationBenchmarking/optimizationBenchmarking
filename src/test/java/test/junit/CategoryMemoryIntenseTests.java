f5b193cea717cb458130316a2b231919e33e6235
thomasWeise <tweise@ustc.edu.cn>
2015-03-22 19:57:07 +0800

    Added Category MemoryIntenseTests to Exclude Tests Needing Too Much Mem
    I added the category "CategoryMemoryIntenseTests" in order to be able
    to exclude JUnit tests needing much memory from the builds. This may be
    needed for CircleCI to build properly.

---
+++ src/test/java/test/junit/CategoryMemoryIntenseTests.java
@@ -0,0 +1,6 @@
+package test.junit;
+
+/** A tag interface for memory-intense tests */
+public interface CategoryMemoryIntenseTests {
+  //
+}
--- src/test/java/test/junit/org/optimizationBenchmarking/utils/text/textOutput/StringWriterTextOutputTest.java
+++ src/test/java/test/junit/org/optimizationBenchmarking/utils/text/textOutput/StringWriterTextOutputTest.java
@@ -2,13 +2,17 @@ package test.junit.org.optimizationBenchmarking.utils.text.textOutput;
 
 import java.io.StringWriter;
 
+import org.junit.experimental.categories.Category;
 import org.optimizationBenchmarking.utils.text.textOutput.AbstractTextOutput;
 import org.optimizationBenchmarking.utils.text.textOutput.ITextOutput;
+
+import test.junit.CategoryMemoryIntenseTests;
 
 /**
  * A test of an internal wrapper class of
  * {@link org.optimizationBenchmarking.utils.text.textOutput.ITextOutput}
  */
+@Category(CategoryMemoryIntenseTests.class)
 public class StringWriterTextOutputTest extends
     TextOutputTest<StringWriter> {
 
