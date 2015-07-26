ebea6d12010a10e1d76ce01f5ac04830f33b1972
thomasWeise <tweise@ustc.edu.cn>
2015-07-26 22:10:35 +0800

    Added Enum to Describe and Detect OS Families
    I added an enum to describe and detect the family of the current
    operating system. So far, it supports Windows, Linux, and MacOS. I will
    probably need it to implement the "invoke-and-wait" special situation
    for Internet Explorer and Opera under Windows, see
    http://stackoverflow.com/questions/3349922

---
+++ src/main/java/org/optimizationBenchmarking/utils/io/EOSFamily.java
@@ -0,0 +1,54 @@
+package org.optimizationBenchmarking.utils.io;
+
+import org.optimizationBenchmarking.utils.text.TextUtils;
+
+/**
+ * An enum of common operating system families. The value of
+ * {@link #DETECTED} corresponds to the family of the OS o the current
+ * computer. This enum has been inspired by
+ * http://stackoverflow.com/questions/228477 and may be refined and
+ * extended in the future by taking into account the values at
+ * http://www.osgi.org/Specifications/Reference.
+ */
+public enum EOSFamily {
+  /** The Apply Mac OS */
+  MacOS,
+  /** Microsoft Windows */
+  Windows,
+  /** Linux */
+  Linux,
+  /** Generic/other/unknown OS: i.e., none of the above */
+  Unknown;
+
+  /** The detected operating system family */
+  public static final EOSFamily DETECTED = EOSFamily.__detect();
+
+  /**
+   * detect the current operating system
+   *
+   * @return the operating system
+   */
+  private static final EOSFamily __detect() {
+    String name;
+
+    try {
+      name = System.getProperty("os.name"); //$NON-NLS-1$
+      if (name != null) {
+        name = TextUtils.toLowerCase(name);
+
+        if (name.contains("mac") || name.contains("darwin")) {//$NON-NLS-1$//$NON-NLS-2$
+          return MacOS;
+        }
+        if (name.contains("win")) {//$NON-NLS-1$
+          return Windows;
+        }
+        if (name.contains("nux")) {//$NON-NLS-1$
+          return Linux;
+        }
+      }
+    } catch (final Throwable error) {
+      // ignore
+    }
+    return Unknown;
+  }
+}
