bd8c2face0a0a106c651a0fe183a43f168f56a09
thomasWeise <tweise@ustc.edu.cn>
2015-08-25 21:04:52 +0800

    `ConfigurationXMLOutput` can now also store `Dump`s
    Just that: The `ConfigurationXMLOutput` tool now takes an `Object` as
    input instead of a `Configuration`. If the object is an instance of
    `Configuration`, then it works as before, i.e., stores it. If the object
    is an instance of `Dump`, it stores the dump. Otherwise, it throws an
    `IllegalArgumentException`.

--- src/main/java/org/optimizationBenchmarking/utils/config/ConfigurationXMLOutput.java
+++ src/main/java/org/optimizationBenchmarking/utils/config/ConfigurationXMLOutput.java
@@ -2,6 +2,7 @@ package org.optimizationBenchmarking.utils.config;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.optimizationBenchmarking.utils.io.encoding.StreamEncoding;
@@ -10,9 +11,12 @@ import org.optimizationBenchmarking.utils.io.structured.impl.abstr.XMLOutputTool
 import org.optimizationBenchmarking.utils.io.xml.XMLBase;
 import org.optimizationBenchmarking.utils.io.xml.XMLElement;
 
-/** the configuration xml output */
-public final class ConfigurationXMLOutput extends
-    XMLOutputTool<Configuration> {
+/**
+ * The configuration xml output can store either
+ * {@link org.optimizationBenchmarking.utils.config.Configuration}s or
+ * {@link org.optimizationBenchmarking.utils.config.Dump}s.
+ */
+public final class ConfigurationXMLOutput extends XMLOutputTool<Object> {
   /** The configuration namespace prefix */
   public static final String CONFIGURATION_NAMESPACE_PREFIX = "cfg"; //$NON-NLS-1$
 
@@ -32,29 +36,60 @@ public final class ConfigurationXMLOutput extends
 
   /** {@inheritDoc} */
   @Override
-  protected final void xml(final IOJob job, final Configuration data,
+  protected final void xml(final IOJob job, final Object data,
       final XMLBase xmlBase) throws Throwable {
+    final Configuration config;
+
     try (XMLElement root = xmlBase.element()) {
       root.namespaceSetPrefix(ConfigurationXML.NAMESPACE_URI,
           ConfigurationXMLOutput.CONFIGURATION_NAMESPACE_PREFIX);
       root.name(ConfigurationXML.NAMESPACE_URI,
           ConfigurationXML.ELEMENT_CONFIGURATION_ROOT);
 
-      synchronized (data.m_data) {
-        for (final Entry<String, Object> e : data.m_data.entries()) {
+      if (data instanceof Configuration) {
+        config = ((Configuration) data);
+
+        synchronized (config.m_data) {
+          for (final Entry<String, Object> e : config.m_data.entries()) {
+            try (final XMLElement param = root.element()) {
+              param.name(ConfigurationXML.NAMESPACE_URI,
+                  ConfigurationXML.ELEMENT_CONFIGURATION_PARAMETER);
+
+              param.attributeEncoded(ConfigurationXML.NAMESPACE_URI,
+                  ConfigurationXML.ATTRIBUTE_CONFIGURATION_PARAMETER_NAME,
+                  e.getKey());
+              param
+                  .attributeEncoded(
+                      ConfigurationXML.NAMESPACE_URI,
+                      ConfigurationXML.ATTRIBUTE_CONFIGURATION_PARAMETER_VALUE,
+                      String.valueOf(e.getValue()));
+            }
+          }
+        }
+        return;
+      }
+
+      if (data instanceof Dump) {
+
+        for (final Map.Entry<Parameter<?>, Object> e : ((Dump) data)) {
           try (final XMLElement param = root.element()) {
             param.name(ConfigurationXML.NAMESPACE_URI,
                 ConfigurationXML.ELEMENT_CONFIGURATION_PARAMETER);
 
             param.attributeEncoded(ConfigurationXML.NAMESPACE_URI,
                 ConfigurationXML.ATTRIBUTE_CONFIGURATION_PARAMETER_NAME,
-                e.getKey());
+                e.getKey().m_name);
             param.attributeEncoded(ConfigurationXML.NAMESPACE_URI,
                 ConfigurationXML.ATTRIBUTE_CONFIGURATION_PARAMETER_VALUE,
                 String.valueOf(e.getValue()));
           }
         }
+        return;
       }
+
+      throw new IllegalArgumentException(//
+          "Can only store configurations or dumps, but received an " //$NON-NLS-1$
+              + data + '.');
     }
   }
 
@@ -66,7 +101,7 @@ public final class ConfigurationXMLOutput extends
 
   /** {@inheritDoc} */
   @Override
-  protected final void file(final IOJob job, final Configuration data,
+  protected final void file(final IOJob job, final Object data,
       final Path file, final StreamEncoding<?, ?> encoding)
       throws Throwable {
     super.file(job, data, file, encoding);
